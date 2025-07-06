2bd789eb235cbf7b6dd1fba250ac54bf8241ffd8
Matheusvfvitor <34576014+Matheusvfvitor@users.noreply.github.com>
2025-07-06 08:11:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ import time
 
 def realizar_login(email, senha, unidade):
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.binary_location = "/usr/bin/chromium"
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    driver = webdriver.Chrome(options=chrome_options)
 
     service = Service(ChromeDriverManager().install())
+    
     driver = webdriver.Chrome(service=service, options=chrome_options)
     wait = WebDriverWait(driver, 20)
 
