ae9d5242763f7870d41b344d79197a5e78788982
Matheusvfvitor <34576014+Matheusvfvitor@users.noreply.github.com>
2025-07-06 07:39:01 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def main():
+    agora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    print(f"Script executado às {agora}")
+
+if __name__ == "__main__":
+    main()
